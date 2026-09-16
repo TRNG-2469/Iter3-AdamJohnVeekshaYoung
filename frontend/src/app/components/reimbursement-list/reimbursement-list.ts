da25a5bce8d4ca68bb730408dc84fe2f9a20ab12
seoky99 <seokyoung99@gmail.com>
2026-09-15 20:40:48 -0400

    integration w/ auth and new main, before new dept filtering

--- frontend/src/app/components/reimbursement-list/reimbursement-list.ts
+++ frontend/src/app/components/reimbursement-list/reimbursement-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 import { ReimbursementComponent } from '../reimbursement-component/reimbursement-component';
 import { Reimbursement } from '../../models/Reimbursement';
 import { Input } from '@angular/core';
@@ -11,65 +11,113 @@ import { ReimbursementService } from '../../services/ReimbursementService';
   templateUrl: './reimbursement-list.html',
   styleUrl: './reimbursement-list.css',
 })
-export class ReimbursementList {
+export class ReimbursementList implements OnInit, OnChanges {
 
-  reimbursements : Reimbursement[] | null = null; 
-  
+  allReimbursements: Reimbursement[]=[];
+
+  reimbursements : Reimbursement[] | null = null;
+
   @Input()
-  currentUser! : User;  
+  currentUser! : User;
 
-  constructor(private reimbursementService : ReimbursementService) {
+  @Input()
+  filtered_status! : string;
+
+  @Input()
+  filtered_department!: string;
+
+  private authorDepartmentMap: { [key: number]: string } = {
+    1: 'Engineering',
+    7: 'Finance',
+  };
+
+  constructor(private reimbursementService : ReimbursementService, private cdr: ChangeDetectorRef) {
 
   }
 
-  //Todo: replace with service once auth finished 
   ngOnInit() {
-    this.reimbursements = [
-    {
-      id: 1,
-      authorId: 1,
-      status: 'APPROVED',
-      amount: 150.00,
-      type: 'LODGING',
-      description: 'Stayed at a nice hotel.',
-      resolverId: null, 
-      submittedAt: '2026-08-25T22:06:01.951029',
-      resolvedAt: null
-    },
-    {
-      id: 2,
-      authorId: 2,
-      status: 'DENIED',
-      amount: 20.75,
-      type: 'FOOD',
-      description: 'Had a little snack.',
-      resolverId: null, 
-      submittedAt: '2026-08-26T16:54:48.857607',
-      resolvedAt: null
-    },
-    {
-      id: 3,
-      authorId: 1,
-      status: 'PENDING',
-      amount: 10.50,
-      type: 'TRANSPORTATION',
-      description: 'Used public bus.',
-      resolverId: null, 
-      submittedAt: '2026-08-25T23:03:28.276475',
-      resolvedAt: null
-    },
-  ];
 
-  /*
     this.reimbursementService.getReimbursements().subscribe({
       next: (reimbursements) => {
-        this.reimbursements = reimbursements;
+
+        this.allReimbursements = reimbursements;
+        this.reimbursements = [...this.allReimbursements];
+        this.cdr.detectChanges();
       },
       error: (err) => {
-        console.log("failed to fetch reimbursements " + err);
+        console.error("FAILED:", err);
       }
-    })
-  */
+    });
+  }
+
+    ngOnChanges(changes: SimpleChanges): void {
+      if (changes['filtered_status'] || changes['filtered_department']){
+      this.applyFilter();
+    }
+  }
+
+  applyFilter() {
+    this.reimbursements = this.allReimbursements.filter(item => {
+      const matchesStatus = !this.filtered_status || item.status === this.filtered_status;
+
+      const departmentName = this.authorDepartmentMap[item.authorId] || '';
+      const matchesDepartment = !this.filtered_department || departmentName === this.filtered_department;
+
+      return matchesStatus && matchesDepartment;
+    });
 
 }
 }
+
+/* Test data 
+
+ /*this.allReimbursements = [
+      {
+        id: 1,
+        authorId: 1,
+        status: 'APPROVED',
+        amount: 150.00,
+        type: 'LODGING',
+        description: 'Stayed at a nice hotel.',
+        resolverId: null,
+        submittedAt: '2026-08-25T22:06:01.951029',
+        resolvedAt: null
+      },
+      {
+        id: 2,
+        authorId: 2,
+        status: 'DENIED',
+        amount: 20.75,
+        type: 'FOOD',
+        description: 'Had a little snack.',
+        resolverId: null,
+        submittedAt: '2026-08-26T16:54:48.857607',
+        resolvedAt: null
+      },
+      {
+        id: 3,
+        authorId: 1,
+        status: 'PENDING',
+        amount: 10.50,
+        type: 'TRANSPORTATION',
+        description: 'Used public bus.',
+        resolverId: null,
+        submittedAt: '2026-08-25T23:03:28.276475',
+        resolvedAt: null
+      },
+
+      {
+        id: 5,
+        authorId: 7,
+        status: 'PENDING',
+        amount: 20.75,
+        type: 'TRANSPORTATION',
+        description: 'Used a taxi.',
+        resolverId: null,
+        submittedAt: '2026-08-25T23:03:28.276475',
+        resolvedAt: null
+      },
+    ]; 
+      this.reimbursements=[...this.allReimbursements]; 
+
+    */
--- frontend/src/app/screens/reimbursements-screen/reimbursements-screen.ts
+++ frontend/src/app/screens/reimbursements-screen/reimbursements-screen.ts
@@ -1,47 +1,40 @@
 import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ReimbursementList } from '../../components/reimbursement-list/reimbursement-list';
-//import { User } from '../../models/User';
+import { User } from '../../models/User';
 import { UserService } from '../../services/UserService';
-import { Router } from '@angular/router';
-import { AuthService } from '../../services/AuthService';
+import {FormsModule} from '@angular/forms';
+import { DepartmentService } from '../../services/DepartmentService';
+import { Department } from '../../models/Department';
 
 @Component({
   selector: 'app-reimbursements-screen',
-  imports: [RouterLink, ReimbursementList],
+  imports: [RouterLink, ReimbursementList, FormsModule],
   templateUrl: './reimbursements-screen.html',
   styleUrl: './reimbursements-screen.css',
 })
-export class ReimbursementsScreen{
+export class ReimbursementsScreen implements OnInit {
 
-  private authService = inject(AuthService);
-  private router = inject(Router);
-  private userService = inject(UserService);
-  //private cdr = inject(ChangeDetectorRef);
+  currentUser : User | null = null;
 
-  currentUser = this.authService.currentUser;
+  departments: Department[]=[];
 
-  logout(): void {
-    this.authService.logout();
-    this.router.navigate(['/']);
+  selected_Status: string='';
+  filtered_status: string ='';
+
+  selected_Department: string = '';
+  filtered_department: string = '';
+
+  private cdr = inject(ChangeDetectorRef);
+  private departmentService=inject(DepartmentService);
+
+
+  constructor(private userService : UserService) {
   }
 
-
   //Todo: replace with service call once auth is finished
-  //ngOnInit() : void {
-     /* this.currentUser = {
-      id : 1,
+  ngOnInit() : void {
 
-      firstName : "jdoe",
-      lastName : "jdoe",
-
-      username : "jdoerocks",
-
-      role: "EMPLOYEE",
-      departmentId : 1
-    } */
-
-      /*
     this.userService.getCurrentUser().subscribe({
       next: (user) => {
         console.log('User successfully fetched:', user);
@@ -52,6 +45,23 @@ export class ReimbursementsScreen{
         console.error('getCurrentUser failed with error:', err);
       }
     });
+
+    this.departmentService.getDepartments().subscribe({
+
+      next: (data) => {
+        console.log('Departments response from backend:', data); // Check F12 console to see the exact keys!
+        this.departments = data;
+        this.cdr.detectChanges()
+      },
+      error: (err) => {
+        console.error('Failed to load departments', err);
+      }
+    });
+
+
   }
-    */
+  applyFilter() {
+    this.filtered_status=this.selected_Status;
+    this.filtered_department = this.selected_Department;
+  }
 }
